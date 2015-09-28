0b66928cb1cfbd372a4d655db0e3063707fcfc23
KarolAdr <adrian.karolak@tieto.com>
2015-09-28 16:04:32 +0200

    Mac addresses in visualizer are printed now

--- visualizer/visualizer.c
+++ visualizer/visualizer.c
@@ -10,9 +10,15 @@
 #include <time.h>                   /* clock(...)        */
 #include <unistd.h>
 #include <string.h>                 /* strerror(..)      */
+#include <net/ethernet.h>           /* h_source, h_dest  */
 #include <errno.h>                  /* errno value       */
 
 int exit_flag = 0;
+char eth_source[] = "[ETH SOURCE ADDRESS]";
+char eth_dest[] = "[ETH DESTINATION ADDRESS]";
+struct ethhdr *eth;
+char mac_addr[64];
+
 static struct traffic_visualizer *create_visualizer() 
 {   
     struct traffic_visualizer *vis = create_traffic_visualizer(32, 1, 2);
@@ -61,6 +67,90 @@ static void monitor(shm_message *segaddr)
     }
 }
 
+static void print_frame()
+{   
+    int count = 0;
+    for (; count < COLS; ++count) {
+        mvprintw(33, count, "-");
+    }
+}
+
+static void print_frame_in()
+{
+    mvprintw(37, 3, eth_source);
+    mvprintw(37, sizeof(eth_source)+8, eth_dest);
+    attron(COLOR_PAIR(3));
+    int count = 0;
+
+    for (; count < 2; ++count) {
+        mvprintw(36+count, 1, "*");
+    }
+    count = 0;
+    for (; count < sizeof(eth_source)+sizeof(eth_dest)+7; ++count) {
+        mvprintw(36, count+1, "*");
+    }
+    count = 0;
+    for (; count < 2; ++count) {
+        mvprintw(36+count, sizeof(eth_source) + sizeof(eth_dest)+8, "*");
+    }
+    attroff(COLOR_PAIR(3));
+}
+
+static void print_frame_out()
+{   
+    int count = 0;
+    mvprintw(37, sizeof(eth_source)+sizeof(eth_dest)+13, eth_source);
+    mvprintw(37, sizeof(eth_source)*2+sizeof(eth_dest)+19, eth_dest);
+    attron(COLOR_PAIR(3));
+    for (; count < 2; ++count) {
+        mvprintw(36+count, sizeof(eth_source) + sizeof(eth_dest)+11, "*");
+    }
+    for (; count < sizeof(eth_source)+sizeof(eth_dest)+10; ++count) {
+        mvprintw(36, sizeof(eth_source)+sizeof(eth_dest)+count+10, "*");
+    }
+    count = 0;
+    for (; count < 2; ++count) {
+        mvprintw(36+count, sizeof(eth_source)*2 +71, "*");
+    }
+    attroff(COLOR_PAIR(3));
+}
+
+static void print_eth_in()
+{   
+    int count = 0;
+    for (; count < sizeof(eth->h_source); ++count) {
+            //mvprintw("%02X", eth->h_source[count]
+            mac_addr[count] = eth->h_source[count];
+            mvprintw(38, (count*3)+5, "%02X", mac_addr[count]);
+            mvprintw(38, (count*3)+7, ":");
+    }
+    count = 0;
+    for (; count < sizeof(eth->h_source); ++count) {
+        //mvprintw("%02X", eth->h_source[count]
+        mac_addr[count] = eth->h_dest[count];
+        mvprintw(38, (count*3)+32, "%02X", mac_addr[count]);
+        mvprintw(38, (count*3)+34, ":");
+    }
+}
+
+static void print_eth_out()
+{   
+    int count = 0;
+    for (; count < sizeof(eth->h_source); ++count) {
+            //mvprintw("%02X", eth->h_source[count]
+            mac_addr[count] = eth->h_source[count];
+            mvprintw(38, (count*3)+62, "%02X", mac_addr[count]);
+            mvprintw(38, (count*3)+64, ":");
+    }
+    count = 0;
+    for (; count < sizeof(eth->h_source); ++count) {
+        //mvprintw("%02X", eth->h_source[count]
+        mac_addr[count] = eth->h_dest[count];
+        mvprintw(38, (count*3)+90, "%02X", mac_addr[count]);
+        mvprintw(38, (count*3)+92, ":");
+    }
+}
+
 static void visualize(struct traffic_visualizer *vis)
 {   
     /* main method in which actual visualizing take place */
@@ -96,12 +186,22 @@ static void visualize(struct traffic_visualizer *vis)
     long prev_time = 0;
     init_pair(1, COLOR_BLACK, COLOR_RED);
     init_pair(2, COLOR_BLACK, COLOR_BLUE);
+    init_pair(3, COLOR_BLUE, COLOR_BLACK);
     shm_message tmp;
     //char index[8];
     //char time_char[8];
+    //char mac_addr[64];
+    memset(mac_addr, 0, sizeof(mac_addr));
+    //char out[] = "[OUTGOING PACKET NUMBER]";
+    //char in[] = "[INCOMING PACKET NUMBER]";
+    //char eth_source[] = "[ETH SOURCE ADDRESS]";
+    //char eth_dest[] = "[ETH DESTINATION ADDRESS]";
+    //struct ethhdr *eth;
+    print_frame();
     while (exit_flag != 1) {
         /* waiting for message from sniffer */
         wait(cond, segaddr);
+            eth = (struct ethhdr*)segaddr[0].buff; 
             tmp = segaddr[0]; /* message arrived, process it */
             if (entry == 0) {
                 entry = 1;
@@ -109,23 +209,15 @@ static void visualize(struct traffic_visualizer *vis)
             }/*process message */
             while ( ( (clock() - prev_time)/1000) < 100) {
                 if (tmp.packet_direction == outgoing) {
-                    /*printf("OUTGOING lkms[%d", tmp.index);
-                    printf("]\n");*/
-                    /*sprintf(index, "%d", tmp.index);
-                    sprintf(time_char, "%d", time);
-                    mvprintw(1, 70, index);
-                    mvprintw(3, 70, time_char);*/
+                    print_frame_out();
+                    print_eth_out();
                     vis->render(vis);
                     vis->set_tx_bitrate(vis, tmp.index, 600);
                     refresh();
                 }
                 else if (tmp.packet_direction == incomming) {
-                    /*printf("INCOMMING lkms[%d", tmp.index);
-                    printf("]\n");*/
-                    /*sprintf(index, "%d", tmp.index);
-                    sprintf(time_char, "%d", time);
-                    mvprintw(1, 60, index);
-                    mvprintw(3, 60, time_char);*/
+                    print_frame_in();
+                    print_eth_in();
                     vis->render(vis);
                     vis->set_rx_bitrate(vis, tmp.index, 600);
                     refresh();
